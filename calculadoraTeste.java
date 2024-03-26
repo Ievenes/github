6e6a86569798bd34a86dc6fbf5370ad2de9ca0d7
Lucas Ievenes <lucas.ratti2009@hotmail.com>
2024-03-25 21:35:43 -0300

    apostila (git-java): funcao subtrair,multiplicar,dividir e raiz implementada e testada

--- calculadoraTeste.java
+++ calculadoraTeste.java
@@ -4,5 +4,18 @@ public class calculadoraTeste {
         //testar soma
         double soma = calc.somar(2, 3);
         System.out.println(soma);
+        //testar subtracao
+        double sub = calc.subtrair(2, 3);
+        System.out.println(sub);
+        //testar multiplicacao
+        double mult = calc.multiplicar(2, 3);
+        System.out.println(mult);
+        //testar divisao
+        double div = calc.divisao(2, 3);
+        System.out.println(div);
+        //testar raiz
+        double raizz = calc.raiz(2, 3);
+        System.out.println(raizz);
+        
     }
 }
