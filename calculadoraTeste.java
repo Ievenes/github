e2d6357827095227953d2d712d817ef60bb50a06
Lucas Ievenes <lucas.ratti2009@hotmail.com>
2024-03-25 21:06:54 -0300

    apostila(git-java): teste da funcao somar

---
+++ calculadoraTeste.java
@@ -0,0 +1,8 @@
+public class calculadoraTeste {
+    public static void main(String[] args) {
+        calculadora calc = new calculadora();
+        //testar soma
+        double soma = calc.somar(2, 3);
+        System.out.println(soma);
+    }
+}
