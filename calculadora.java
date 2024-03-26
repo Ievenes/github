777768ed9548d55411ea36d853b12600697980f3
Lucas Ievenes <lucas.ratti2009@hotmail.com>
2024-03-25 21:38:58 -0300

    apostila(git-java): classes de subtracao, divisao, multiplicacao e raiz criada

--- calculadora.java
+++ calculadora.java
@@ -11,4 +11,10 @@ public class calculadora {
     public double multiplicar(double a, double b){
         return a * b;
     }
+    public double divisao(double a, double b){
+        return a / b;
+    }
+    public double raiz(double a, double b){
+        return Math.pow(a,b);
+    }
 }
