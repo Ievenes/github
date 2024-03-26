964ad347352dd29b31458d93b81a8919bd06ad60
Lucas Ievenes <lucas.ratti2009@hotmail.com>
2024-03-25 21:25:53 -0300

    apostila(git-java): função de subtracao e multiplicacao implementada

--- calculadora.java
+++ calculadora.java
@@ -5,4 +5,10 @@ public class calculadora {
     public double somar(double a, double b){
         return a + b;
     }
+    public double subtrair(double a, double b){
+        return a - b;
+    }
+    public double multiplicar(double a, double b){
+        return a * b;
+    }
 }
