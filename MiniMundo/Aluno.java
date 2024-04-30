49b2a74e489b10d45da39113812b431488c22bc7
Lucas Ievenes Ratti <61846972+Ievenes@users.noreply.github.com>
2024-04-29 21:11:18 -0300

    Ai pai para

---
+++ MiniMundo/Aluno.java
@@ -0,0 +1,12 @@
+public class Aluno extends Pessoa{
+    int ra;
+
+    public int getRa() {
+        return this.ra;
+    }
+
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+
+}
---
+++ MiniMundo/TesteLecionar.java
@@ -0,0 +1,10 @@
+public class TesteLecionar {
+    public static void main(String[] args) {
+        ProfessorHorista professorHorista = new ProfessorHorista();
+        professorHorista.setNome("Bonato");
+        ProfessorPesquisador professorPesquisador = new ProfessorPesquisador();
+        professorPesquisador.setNome("Anderson");
+        professorHorista.lecionar();
+        professorPesquisador.lecionar();
+    }
+}
